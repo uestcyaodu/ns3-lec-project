58665cbcb746af6547095abb76df1784f69885d2
uestcyaodu <duyao@cumt.edu.cn>
2016-08-28 16:30:41 +0800

    Project_3

---
+++ pro_3.cc
@@ -0,0 +1,253 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+
+// Network Topology
+// Number of  each section's wifi or csma nodes can be increased up to 250
+//                     |                                  |
+//         Section 1   |                                  |      Section 2
+// --------------------|----------------------------------|--------------------
+//     Wifi 10.1.2.0                                           Wifi 10.1.3.0
+//                       AP                            AP
+//  *      *      *      *                             *      *      *      *
+//  |      |      |      |          10.1.1.0           |      |      |      |
+// n6      n7     n8     n0 ========== n1 =========== n2     n3     n4     n5
+//                                   LAN
+//
+//echo_server在Section 1的10.1.2.3 ; 端口号为6 ; 客户端安装于Section 2的所有wifi_STA上以及LAN中的设备n1上
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("ThirdScriptExample");		//定义记录组件
+
+int
+main (int argc, char *argv[])
+{
+  bool verbose = true;
+  uint32_t nWifi_1 = 3;			//wifi 区1节点数量
+  uint32_t nWifi_2 = 3;
+  uint32_t nCsma = 3;		 	  //wifi 区2节点数量
+   bool tracing = true;
+
+
+  CommandLine cmd;
+  cmd.AddValue ("nWifi_1", "Number of section 1 's wifi STA devices", nWifi_1);
+  cmd.AddValue ("nWifi_2", "Number of section 2 's wifi STA devices", nWifi_2);
+  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+
+  cmd.Parse (argc,argv);
+
+  if (nWifi_1 > 250 || nWifi_2 > 250 || nCsma > 250)
+    {
+      std::cout << "Too many wifi or csma nodes, no more than 250 in each section." << std::endl;
+      return 1;
+    }
+
+  if (verbose)
+    {
+        LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_FUNCTION);
+        LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_FUNCTION);	//启动记录组件
+    }
+
+// csma
+  //创建csma节点
+  NodeContainer csmaNodes;
+  csmaNodes.Create (nCsma);
+
+  //创建信道，设置信道参数，在设备安装到节点上
+  CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+
+  NetDeviceContainer csmaDevices;
+  csmaDevices = csma.Install (csmaNodes);
+
+// section 1 wifi
+//创建wifista无线终端，AP接入点
+  NodeContainer wifiStaNodes_1;
+  wifiStaNodes_1.Create (nWifi_1);
+  NodeContainer wifiApNode_1 = csmaNodes.Get (0);
+
+  //创建无线设备于无线节点之间的互联通道，并将通道对象与物理层对象关联
+  //确保所有物理层对象使用相同的底层信道，即无线信道
+  YansWifiChannelHelper channel_1 = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy_1 = YansWifiPhyHelper::Default ();
+  phy_1.SetChannel (channel_1.Create ());
+
+  //配置速率控制算法，AARF算法
+  WifiHelper wifi_1 = WifiHelper::Default ();
+  wifi_1.SetRemoteStationManager ("ns3::AarfWifiManager");
+
+  NqosWifiMacHelper mac_1 = NqosWifiMacHelper::Default ();
+
+  //配置mac类型为sta模式，不发送探测请求
+  Ssid ssid_1 = Ssid ("ns-3-ssid-section-1");
+  mac_1.SetType ("ns3::StaWifiMac",
+               "Ssid", SsidValue (ssid_1),
+               "ActiveProbing", BooleanValue (false));
+
+  //创建无线设备，将mac层和phy层安装到设备上
+  NetDeviceContainer staDevices_1;
+  staDevices_1 = wifi_1.Install (phy_1, mac_1, wifiStaNodes_1);
+
+  //配置AP节点的mac层为AP模式，创建AP设备
+  mac_1.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue (ssid_1));
+
+  NetDeviceContainer apDevices_1;
+  apDevices_1 = wifi_1.Install (phy_1, mac_1, wifiApNode_1);
+
+// section 2 wifi
+//创建wifista无线终端，AP接入点
+  NodeContainer wifiStaNodes_2;
+  wifiStaNodes_2.Create (nWifi_2);
+  NodeContainer wifiApNode_2 = csmaNodes.Get (nCsma - 1);
+
+  //创建无线设备于无线节点之间的互联通道，并将通道对象与物理层对象关联
+  //确保所有物理层对象使用相同的底层信道，即无线信道
+  YansWifiChannelHelper channel_2 = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy_2 = YansWifiPhyHelper::Default ();
+  phy_2.SetChannel (channel_2.Create ());
+
+  //配置速率控制算法，AARF算法
+  WifiHelper wifi_2 = WifiHelper::Default ();
+  wifi_2.SetRemoteStationManager ("ns3::AarfWifiManager");
+
+  NqosWifiMacHelper mac_2 = NqosWifiMacHelper::Default ();
+
+  //配置mac类型为sta模式，不发送探测请求
+  Ssid ssid_2 = Ssid ("ns-3-ssid-section-2");
+  mac_2.SetType ("ns3::StaWifiMac",
+               "Ssid", SsidValue (ssid_2),
+               "ActiveProbing", BooleanValue (false));
+
+  //创建无线设备，将mac层和phy层安装到设备上
+  NetDeviceContainer staDevices_2;
+  staDevices_2 = wifi_2.Install (phy_2, mac_2, wifiStaNodes_2);
+
+  //配置AP节点的mac层为AP模式，创建AP设备
+  mac_2.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue (ssid_2));
+
+  NetDeviceContainer apDevices_2;
+  apDevices_2 = wifi_2.Install (phy_2, mac_2, wifiApNode_2);
+
+
+  //配置移动模型，起始位置
+  MobilityHelper mobility;
+
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (25.0),
+                                 "DeltaY", DoubleValue (25.0),
+                                 "GridWidth", UintegerValue (3),
+                                 "LayoutType", StringValue ("RowFirst"));
+  //配置STA移动方式，两区STA设置为两种随机运动模型：RandomWalk2dMobilityModel、RandomDirection2dMobilityModel
+   mobility.SetMobilityModel (
+   "ns3::RandomWalk2dMobilityModel",
+   "Bounds", RectangleValue (Rectangle (-100, 100, -100, 100))
+   );
+   mobility.Install (wifiStaNodes_1);
+
+   mobility.SetMobilityModel (
+   "ns3::RandomDirection2dMobilityModel",
+   "Bounds", RectangleValue (Rectangle (-100, 100, -100, 100)),
+   "Speed", StringValue ("ns3::UniformRandomVariable[Min=1.0|Max=2.0]"),
+   "Pause", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]")
+   );
+   mobility.Install (wifiStaNodes_2);
+
+  //配置csmanodes(包括了APnodes)移动方式，ConstantPositionMobilityModel，固定位置模型
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (csmaNodes);
+  //已经创建了节点，设备，信道和移动模型，接下来配置协议栈
+  InternetStackHelper stack;
+  stack.Install (wifiStaNodes_1);
+  stack.Install (wifiStaNodes_2);
+  stack.Install (csmaNodes);
+
+  //分配IP地址
+  Ipv4AddressHelper address;
+ //csma信道
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer csmaInterfaces;
+  csmaInterfaces = address.Assign (csmaDevices);
+  //wifi信道
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer wifi_1Interfaces;
+  wifi_1Interfaces = address.Assign (staDevices_1);
+  address.Assign (apDevices_1);
+
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer wifi_2Interfaces;
+  wifi_2Interfaces = address.Assign (staDevices_2);
+  address.Assign (apDevices_2);
+
+  //放置echo服务端程序在Section 1 最右边的wifi节点,端口为6
+  UdpEchoServerHelper echoServer (6);
+
+  ApplicationContainer serverApps = echoServer.Install (wifiStaNodes_1.Get (nWifi_1 - 1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  //回显客户端放在最后的STA节点，指向Wifi网络服务器上面的节点地址，端口为6
+  UdpEchoClientHelper echoClient (wifi_1Interfaces.GetAddress (nWifi_1 - 1), 6);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  //安装其他节点应用程序
+  ApplicationContainer clientApps =
+  echoClient.Install (csmaNodes.Get (1));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  for(int i = 0; i < (int)nWifi_2; i++){
+  ApplicationContainer clientApps_1 = echoClient.Install (wifiStaNodes_2.Get (nWifi_2 - 1 - i));
+  clientApps_1.Start (Seconds (3.0+i));
+  clientApps_1.Stop (Seconds (10.0));
+  }
+
+  //启动互联网络路由
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  Simulator::Stop (Seconds (10.0));
+
+  if (tracing == true)
+    {
+      AsciiTraceHelper ascii;
+      phy_1.EnableAsciiAll(ascii.CreateFileStream("pro_3_wifi_1.tr"));
+      phy_1.EnablePcapAll ("pro_3_wifi_1");
+      phy_2.EnableAsciiAll(ascii.CreateFileStream("pro_3_wifi_2.tr"));
+      phy_2.EnablePcapAll ("pro_3_wifi_2");
+      csma.EnableAsciiAll(ascii.CreateFileStream("pro_3_csma.tr"));
+      csma.EnablePcapAll ("pro_3_csma");
+    }
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
---
+++ report_pro_3.md
@@ -0,0 +1,65 @@
+#Project 2
+###17级 杜耀
+---
+##Project要求
+以各自的Project-2.cc为示例，并在新的要求下完成任务
+- 使用Logging系统记录错误信息，学会如何进行调试
+- 使用Tracing系统，针对至少两种信道，输出Ascii与Pcap两种文件
+- 自行安装PyViz，使用PyViz查看Project-2仿真过程
+- 自行安装Wireshark，熟悉学会使用TcpDump和Wireshark查看文件
+
+###Logging系统
+![Image of pro_2.1 logging]
+(http://ww2.sinaimg.cn/large/005unglEgw1f79i19erv0j30kw0sdtfv.jpg)
+
+```C++
+if (verbose)
+  {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_FUNCTION);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_FUNCTION);	//启动记录组件
+  }
+```
+- 修改pro_2.1 更改LOG_LEVEL_INFO为LOG_LEVEL_FUNCTION,记录更加详细的参数
+- NS_LOG_FUNCTION——Log a message describing each function called. 记录描述每个调用函数信息
+
+###Tracing系统
+![Image of ‘.tr’ ASCII file]
+(http://ww3.sinaimg.cn/large/005unglEgw1f79i78z5bvj31250m7gwi.jpg)
+
+```C++
+if (tracing == true)
+  {
+    AsciiTraceHelper ascii;
+    phy_1.EnableAsciiAll(ascii.CreateFileStream("pro_3_wifi_1.tr"));
+    phy_1.EnablePcapAll ("pro_3_wifi_1");
+    phy_2.EnableAsciiAll(ascii.CreateFileStream("pro_3_wifi_2.tr"));
+    phy_2.EnablePcapAll ("pro_3_wifi_2");
+    csma.EnableAsciiAll(ascii.CreateFileStream("pro_3_csma.tr"));
+    csma.EnablePcapAll ("pro_3_csma");
+  }
+```
+- 使用AsciiTraceHelper类，结合ascii.CreateFileStream,生成tracing文件
+- 打开csma.tr文件后，即可查看完整的ASCII格式的链路信息
+- 生成共22个数据文件（包括 .tr 文件 和 .pcap 文件）已整合在 “Project3_data.zip”中
+
+###PyViz动态展示
+- pro_2.1.cc：
+![Image of pro_2.1 PyViz]
+(http://ww3.sinaimg.cn/large/005unglEgw1f79i18mpqqj30o20iqq6e.jpg)
+
+- pro_2.2.cc：
+![Image of pro_2.2 PyViz]
+(http://ww4.sinaimg.cn/large/005unglEgw1f79i180mq5j30o90iqgq8.jpg)
+
+###Wireshark抓包
+![Image of Wireshark]
+(http://ww3.sinaimg.cn/large/005unglEgw1f79i17ftrij30qu0olwhy.jpg)
+- 如图清楚地展示了pro_2.1.cc仿真运行中UDP数据包的流向，通过wireshark可抓包查看数据包的内容、起始地址、目的地址、长度等信息
+
+###安装代码：
+- PyViz: sudo apt-get install python-dev python-pygraphviz python-kiwi python-pygoocanvas python-gnome2 python-gnomedesktop python-rsvg
+- Wireshark:sudo apt-get install wireshark (手动安装方法详见：http://blog.csdn.net/bo5509/article/details/8189757)
+
+###总结
+- 本次Project着重训练了监测仿真过程及处理仿真数据的技术
+- 多角度观测仿真过程，利于进一步修改仿真代码
